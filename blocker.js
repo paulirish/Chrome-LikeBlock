708d9bc2d0c47e04b698bae98d652dac11184c9a
Gianni Chiappetta <gianni@runlevel6.org>
2010-05-03 19:40:53 -0400

    updated: Iterate through the iframe array only once.

--- blocker.js
+++ blocker.js
@@ -15,15 +15,12 @@
     head.appendChild(ss);
 
     // for some reason qSA throws an error on selecting iframes by src.
-    Array.prototype.slice.call(document.querySelectorAll('iframe')).filter(function(v,k,arra){
-      if (v.src.indexOf('http://www.facebook.com/plugins/like.php') === 0) return true;
-    }).forEach(function(v,k,arr){
-        
+    Array.prototype.slice.call(document.querySelectorAll('iframe')).forEach(function(v,k,arra){
+      if (v.src.indexOf('http://www.facebook.com/plugins/like.php') === 0) {
         // kill the iframe to avoid the 12 resources from downloading.
         // thx @raycmorgan
         // Yes, that's 12 requests PER like button: http://bit.ly/fblikeperfisbad (see Act Three)
         v.parentNode.removeChild(v);
-    })
+      }
+    });
     
-   
-   
