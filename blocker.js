ee00093fce94a409d3c570ab9115200534c152e1
Paul Irish <paul.irish@gmail.com>
2010-05-03 16:54:31 -0400

    remove iframes completely. v0.1.2

--- blocker.js
+++ blocker.js
@@ -2,7 +2,7 @@
 // @name          LikeBlock
 // @namespace     http://www.paulirish.com
 // @description	  Blocks Facebook Like buttons
-// @version 0.1.1
+// @version 0.1.2
 // @include       http://*
 // ==/UserScript==
 
@@ -14,13 +14,16 @@
     ss.innerHTML = 'iframe[src^="http://www.facebook.com/plugins/like.php"] { display: none; }';
     head.appendChild(ss);
 
-
-   
-   /*
-    // querySelectorAll doesnt like looking at iframe src's so this is my faux selector.
+    // for some reason qSA throws an error on selecting iframes by src.
     Array.prototype.slice.call(document.querySelectorAll('iframe')).filter(function(v,k,arra){
       if (v.src.indexOf('http://www.facebook.com/plugins/like.php') === 0) return true;
-    });
-    */
+    }).forEach(function(v,k,arr){
+        
+        // kill the iframe to avoid the 12 resources from downloading.
+        // thx @raycmorgan
+        // Yes, that's 12 requests PER like button: http://bit.ly/fblikeperfisbad (see Act Three)
+        v.parentNode.removeChild(v);
+    })
+    
    
    
