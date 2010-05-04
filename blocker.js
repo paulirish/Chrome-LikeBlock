e4dff8043633faceb16ea645d8f73fa7df41732b
Paul Irish <paul.irish@gmail.com>
2010-05-04 15:37:22 -0400

    fix for alternate Like URL. (thx infdaze)

--- blocker.js
+++ blocker.js
@@ -2,7 +2,7 @@
 // @name          LikeBlock
 // @namespace     http://www.paulirish.com
 // @description	  Blocks Facebook Like buttons
-// @version 0.1.2
+// @version 0.1.3
 // @include       http://*
 // ==/UserScript==
 
@@ -11,12 +11,13 @@
     var ss      = document.createElement('style'),
         head    = document.head || document.querySelector('head');
           
-    ss.innerHTML = 'iframe[src^="http://www.facebook.com/plugins/like.php"] { display: none; }';
+    ss.innerHTML = 'iframe[src^="http://www.facebook.com/plugins/like.php"], iframe[src^="http://www.facebook.com/widgets/like.php"] { display: none; }';
     head.appendChild(ss);
 
     // for some reason qSA throws an error on selecting iframes by src.
     Array.prototype.slice.call(document.querySelectorAll('iframe')).forEach(function(v,k,arra){
-      if (v.src.indexOf('http://www.facebook.com/plugins/like.php') === 0) {
+      if (v.src.indexOf('http://www.facebook.com/plugins/like.php') === 0 ||
+          v.src.indexOf('http://www.facebook.com/widgets/like.php') === 0) {
         // kill the iframe to avoid the 12 resources from downloading.
         // thx @raycmorgan
         // Yes, that's 12 requests PER like button: http://bit.ly/fblikeperfisbad (see Act Three)
