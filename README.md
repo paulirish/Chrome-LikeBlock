5be44efbeca40b4f0654f99be03d719941663c04
Paul Irish <paul.irish@gmail.com>
2010-05-12 18:02:01 -0400

    docs. greasemonkey version!

---
+++ README.md
@@ -0,0 +1,25 @@
+Chrome LikeBlock
+================
+
+Raise your hand if you like the Facebook Like button!
+... yeah didn't think so.
+
+http://blogs.webtrends.com/blog/2010/05/12/think-before-you-like/ describes how you're giving away permission to post stuff to your own news feed
+
+http://bit.ly/fblikeperfisbad (see Act Three) describes how the FB Like button is terrible from a page performance standpoint.
+
+Banish thee Like buttons!!!
+---------------------------
+
+Install here (extra points for pronouncing the URL)
+https://chrome.google.com/extensions/detail/kdlggbdofhjlkfhcfdchmkjjbnkamhpb/ 
+
+
+You can also install it as a *greasemonkey script in Firefox* by opening the `likeblock.user.js`
+
+
+
+
+![60 Minute SPEED PROJECT approved.](http://www.fffff.at/widgets/speed-project/images/time_60.png)
+
+
--- likeblock.user.js
+++ likeblock.user.js
@@ -2,25 +2,29 @@
 // @name          LikeBlock
 // @namespace     http://www.paulirish.com
 // @description	  Blocks Facebook Like buttons
-// @version 0.1.3
+// @version 0.1.4
 // @include       http://*
 // ==/UserScript==
 
-
+// yes, this file is installable as a greasemonkey script (in firefox)
 
     var ss      = document.createElement('style'),
         head    = document.head || document.querySelector('head');
           
-    ss.innerHTML = 'iframe[src^="http://www.facebook.com/plugins/like.php"], iframe[src^="http://www.facebook.com/widgets/like.php"] { display: none; }';
+    ss.innerHTML = 'iframe[src^="http://www.facebook.com/plugins/like.php"],\
+                    iframe[src^="http://www.facebook.com/widgets/like.php"],\
+                    { display: none; }';
     head.appendChild(ss);
 
     // for some reason qSA throws an error on selecting iframes by src.
     Array.prototype.slice.call(document.querySelectorAll('iframe')).forEach(function(v,k,arra){
       if (v.src.indexOf('http://www.facebook.com/plugins/like.php') === 0 ||
           v.src.indexOf('http://www.facebook.com/widgets/like.php') === 0) {
+            
         // kill the iframe to avoid the 12 resources from downloading.
         // thx @raycmorgan
-        // Yes, that's 12 requests PER like button: http://bit.ly/fblikeperfisbad (see Act Three)
+        // Yes, that's 12 requests PER like button: 
+        //     http://bit.ly/fblikeperfisbad (see Act Three)
         v.parentNode.removeChild(v);
       }
     });
